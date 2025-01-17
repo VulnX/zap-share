f2d499c907f6f1ffe11b70e95d9c83d3e0fbbb96
VulnX <vulnx101@gmail.com>
2025-01-17 12:37:20 +0530

    send file

--- src-tauri/src/api.rs
+++ src-tauri/src/api.rs
@@ -1,27 +1,37 @@
 use crate::server;
+use actix_web::dev::ServerHandle;
+use serde::Serialize;
 use std::{
-    io::{stdin, stdout, Read, Write},
-    sync::mpsc,
+    path::PathBuf,
+    sync::{mpsc, Mutex},
     thread,
     time::Duration,
 };
+use tauri::Runtime;
+
+pub static SERVER_HANDLE: Mutex<Option<ServerHandle>> = Mutex::new(None);
 
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct Url {
     ip: Option<String>,
-    port: u16
+    port: u16,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub enum StartServerResponse {
     Success(Url),
     Error(String),
 }
 
+#[derive(Clone)]
+pub enum TransferMode {
+    Send(PathBuf),
+    Receive,
+}
 
 /// Starts server in `send` mode
-/// 
+///
 /// Assumes filepath is a valid path to the user selected file, or a content URI in case of android.
 ///
 /// Return value:
@@ -33,30 +43,35 @@ pub enum StartServerResponse {
 ///   }
 /// }
 /// ```
-/// 
+///
 /// or
-/// 
+///
 /// ```javascript
 /// { "Error": "<error message>" }
 /// ```
+#[allow(dead_code)]
 #[tauri::command]
-pub async fn send_file(filepath: String) -> StartServerResponse {
-    dbg!(filepath);
-    start_server()
+pub fn send_file<R: Runtime>(window: tauri::Window<R>, filepath: String) -> StartServerResponse {
+    let filepath = PathBuf::from(filepath);
+    if !filepath.is_file() {
+        return StartServerResponse::Error("No such file exists".into());
+    }
+    let mode = TransferMode::Send(filepath);
+    start_server(window, mode)
 }
 
-pub fn pause() {
-    let mut stdout = stdout();
-    stdout.write(b"Press Enter to continue...").unwrap();
-    stdout.flush().unwrap();
-    stdin().read(&mut [0]).unwrap();
-}
+// pub fn pause() {
+//     let mut stdout = stdout();
+//     stdout.write(b"Press Enter to continue...").unwrap();
+//     stdout.flush().unwrap();
+//     stdin().read(&mut [0]).unwrap();
+// }
 
 /// Starts (or re-starts existing) actix web server in separate thread
-fn start_server() -> StartServerResponse {
+fn start_server<R: Runtime>(window: tauri::Window<R>, mode: TransferMode) -> StartServerResponse {
     let (tx, rx) = mpsc::channel::<u16>();
     thread::spawn(|| {
-        server::start_server(tx);
+        server::start_server(window, mode, tx);
     });
     let port;
     match rx.recv_timeout(Duration::from_secs(10)) {
@@ -64,8 +79,5 @@ fn start_server() -> StartServerResponse {
         Err(_) => return StartServerResponse::Error("Timeout: Failed to start server".into()),
     }
     let ip = local_ip_address::local_ip().ok().map(|ip| ip.to_string());
-    StartServerResponse::Success(Url {
-        ip,
-        port
-    })
+    StartServerResponse::Success(Url { ip, port })
 }
--- src-tauri/src/server.rs
+++ src-tauri/src/server.rs
@@ -1,15 +1,90 @@
-use actix_web::{rt, web, App, HttpResponse, HttpServer, Responder};
-use std::sync::mpsc;
+use actix_web::{
+    body::SizedStream,
+    http::header::{ContentDisposition, ContentType},
+    rt, web, App, HttpResponse, HttpServer, Responder,
+};
+use futures_util::stream;
+use std::{io::Error, path::PathBuf, sync::mpsc};
+use tauri::{Emitter, Runtime, Window};
+use tokio::io::AsyncReadExt;
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("it's working")
+use crate::api::{TransferMode, SERVER_HANDLE};
+
+const CHUNK_SIZE: usize = 1024 * 1024; // 1 MiB
+
+// TODO : Refactor this entire function
+async fn download(filepath: web::Data<PathBuf>, window: web::Data<Window>) -> impl Responder {
+    let filepath = &**filepath.clone();
+    let file_name = filepath.file_name().unwrap().to_str().unwrap();
+    let file = tokio::fs::File::open(filepath)
+        .await
+        .expect("successfully open selected file");
+    let file_size = file.metadata().await.unwrap().len();
+    let transferred: usize = 0;
+    let progress = 0;
+    let data_stream = stream::unfold(
+        (file, transferred, progress, window),
+        move |(mut file, mut transferred, mut progress, window)| async move {
+            let mut chunk = vec![0; CHUNK_SIZE];
+            match file.read(&mut chunk).await {
+                Ok(0) => None,
+                Ok(n) => {
+                    chunk.truncate(n);
+                    transferred += n;
+                    dbg!(transferred);
+                    let new_progress = transferred * 100 / file_size as usize;
+                    if new_progress > progress {
+                        progress = new_progress;
+                        window.emit("progress-update", progress).unwrap();
+                    }
+                    Some((
+                        Ok::<_, Error>(web::Bytes::from(chunk)),
+                        (file, transferred, progress, window),
+                    ))
+                }
+                Err(e) => {
+                    panic!("Error occured while reading file: {e}")
+                }
+            }
+        },
+    );
+    HttpResponse::Ok()
+        .insert_header(ContentType::octet_stream())
+        .insert_header(ContentDisposition::attachment(file_name))
+        .body(SizedStream::new(file_size, data_stream))
 }
 
-pub fn start_server(tx: mpsc::Sender<u16>) {
+#[allow(unreachable_code)]
+async fn upload() -> impl Responder {
+    unimplemented!();
+    HttpResponse::Ok().body("unimplemented")
+}
+
+pub fn start_server<R: Runtime>(
+    window: tauri::Window<R>,
+    mode: TransferMode,
+    tx: mpsc::Sender<u16>,
+) {
     let server;
+    let window = web::Data::new(window);
     loop {
-        let _server = HttpServer::new(|| App::new().route("/", web::get().to(index)));
+        let mode = mode.clone();
+        let window = web::Data::new(window.clone());
+        let _server = HttpServer::new(move || {
+            let mut app = App::new();
+            match &mode {
+                TransferMode::Send(filepath) => {
+                    app = app
+                        .route("/download", web::get().to(download))
+                        .app_data(web::Data::new(filepath.clone()))
+                }
+                TransferMode::Receive => app = app.route("/upload", web::get().to(upload)),
+            };
+            app = app.app_data(window.clone());
+            app
+        });
         if let Ok(_server) = _server.bind(("0.0.0.0", 0)) {
+            // port is `0` to allow automatic assigning of random port
             server = _server;
             break;
         }
@@ -22,6 +97,12 @@ pub fn start_server(tx: mpsc::Sender<u16>) {
         .expect("valid address from server")
         .port();
     tx.send(port).unwrap();
-
-    rt::System::new().block_on(server.run()).unwrap();
+    let server = server.shutdown_timeout(0).run();
+    let mut guard = SERVER_HANDLE.lock().unwrap();
+    *guard = Some(server.handle());
+    // This function will be running as long as the server is, thus guard
+    // won't be dropped automatically. To make it available for other
+    // threads, we need to manually drop it here.
+    drop(guard);
+    rt::System::new().block_on(server).unwrap();
 }
