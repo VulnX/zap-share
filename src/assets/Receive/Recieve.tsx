7e4bf744368031b24686e463be48c108ec1ad911
Omkar <kaamkijiye@gmail.com>
2025-02-24 23:05:17 +0530

    Fixed minor frontend issues

--- src/assets/Receive/Recieve.tsx
+++ src/assets/Receive/Recieve.tsx
@@ -10,7 +10,7 @@ export default function Recieve() {
     return (
         <div className={`min-h-screen flex flex-col  ${isTheme ? ' bg-dark-background' : ' bg-light-background '}`}>
             <nav className="flex items-center justify-between || h-[20vh]">
-                <svg xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 -960 960 960" width="35px" fill={isTheme ? '#FFFFFF' : ' #000000'} className={`ml-10 rounded-full ${isTheme ? 'hover:bg-zinc-700' : 'hover:bg-neutral-200'}`}
+                <svg xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 -960 960 960" width="35px" fill={isTheme ? '#FFFFFF' : ' #000000'} className={`ml-10 rounded-full ${isTheme ? 'hover:bg-zinc-700' : 'hover:bg-neutral-200'} clickable`}
                     onClick={() => navigate('/', { replace: true })}>
                     <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" />
                 </svg>
