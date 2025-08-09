51891e1672e981c0b31bed339e5cfcbe161e02da
VulnX <vulnx101@gmail.com>
2025-08-09 07:20:40 +0530

    Add: plugin init

---
+++ tauri-plugin-ipd/src/desktop.rs
@@ -0,0 +1,22 @@
+use serde::de::DeserializeOwned;
+use tauri::{plugin::PluginApi, AppHandle, Runtime};
+
+use crate::models::*;
+
+pub fn init<R: Runtime, C: DeserializeOwned>(
+  app: &AppHandle<R>,
+  _api: PluginApi<R, C>,
+) -> crate::Result<Ipd<R>> {
+  Ok(Ipd(app.clone()))
+}
+
+/// Access to the ipd APIs.
+pub struct Ipd<R: Runtime>(AppHandle<R>);
+
+impl<R: Runtime> Ipd<R> {
+  pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
+    Ok(PingResponse {
+      value: payload.value,
+    })
+  }
+}
---
+++ tauri-plugin-ipd/src/lib.rs
@@ -0,0 +1,48 @@
+use tauri::{
+  plugin::{Builder, TauriPlugin},
+  Manager, Runtime,
+};
+
+pub use models::*;
+
+#[cfg(desktop)]
+mod desktop;
+#[cfg(mobile)]
+mod mobile;
+
+mod commands;
+mod error;
+mod models;
+
+pub use error::{Error, Result};
+
+#[cfg(desktop)]
+use desktop::Ipd;
+#[cfg(mobile)]
+use mobile::Ipd;
+
+/// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the ipd APIs.
+pub trait IpdExt<R: Runtime> {
+  fn ipd(&self) -> &Ipd<R>;
+}
+
+impl<R: Runtime, T: Manager<R>> crate::IpdExt<R> for T {
+  fn ipd(&self) -> &Ipd<R> {
+    self.state::<Ipd<R>>().inner()
+  }
+}
+
+/// Initializes the plugin.
+pub fn init<R: Runtime>() -> TauriPlugin<R> {
+  Builder::new("ipd")
+    .invoke_handler(tauri::generate_handler![commands::ping])
+    .setup(|app, api| {
+      #[cfg(mobile)]
+      let ipd = mobile::init(app, api)?;
+      #[cfg(desktop)]
+      let ipd = desktop::init(app, api)?;
+      app.manage(ipd);
+      Ok(())
+    })
+    .build()
+}
