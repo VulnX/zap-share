2ff426c18b000672a940b2a3877c5697564a1434
VulnX <vulnx101@gmail.com>
2025-10-01 17:33:10 +0530

    fix: heading

--- src/local/Choice/Navigation.tsx
+++ src/local/Choice/Navigation.tsx
@@ -124,7 +124,7 @@ export function LogoTitle() {
             isTheme ? " text-light-text" : "text-black "
           }`}
         >
-          Lightning-fast sharing, minus the drama!
+          Prathmesh commit kyu nahi kiya ab tak
         </p>
       </div>
     </div>
