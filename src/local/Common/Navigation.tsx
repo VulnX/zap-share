609f084602e3f090c568f9595960e1a7ae264d7e
VulnX <vulnx101@gmail.com>
2026-03-27 18:24:27 +0530

    fix: navbar icon spacing

--- src/local/Common/Navigation.tsx
+++ src/local/Common/Navigation.tsx
@@ -158,17 +158,12 @@ export default function Navigation({
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
+            viewBox="0 0 640 640"
+            height="100%"
+            fill={tab === "recv" ? "black" : "white"}
+            className="mr-3"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-            />
+            <path d="M568.4 37.7C578.2 34.2 589 36.7 596.4 44C603.8 51.3 606.2 62.2 602.7 72L424.7 568.9C419.7 582.8 406.6 592 391.9 592C377.7 592 364.9 583.4 359.6 570.3L295.4 412.3C290.9 401.3 292.9 388.7 300.6 379.7L395.1 267.3C400.2 261.2 399.8 252.3 394.2 246.7C388.6 241.1 379.6 240.7 373.6 245.8L261.2 340.1C252.1 347.7 239.6 349.7 228.6 345.3L70.1 280.8C57 275.5 48.4 262.7 48.4 248.5C48.4 233.8 57.6 220.7 71.5 215.7L568.4 37.7z" />
           </svg>
           Send
         </div>
@@ -182,7 +177,7 @@ export default function Navigation({
             viewBox="0 0 24 24"
             strokeWidth="1.5"
             stroke="currentColor"
-            className="size-6"
+            className="size-6 mr-3"
           >
             <path
               strokeLinecap="round"
